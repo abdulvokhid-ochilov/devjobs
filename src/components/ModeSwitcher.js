30c2e1f014651c4e01eae1092aca2098993afa44
Abdulvokhid <mr.ochilov1997@gmail.com>
2021-12-26 00:32:00 +0900

    Dark mode switcher

--- src/components/ModeSwitcher.js
+++ src/components/ModeSwitcher.js
@@ -3,20 +3,29 @@ import { ReactComponent as Moon } from "../assets/desktop/icon-moon.svg";
 import { ReactComponent as Sun } from "../assets/desktop/icon-sun.svg";
 
 const ModeSwitcher = () => {
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState();
 
   const toggleClass = " transform translate-x-[24px]";
 
   useEffect(() => {
-    if (localStorage.theme === false) {
-      setToggle(false);
-      document.documentElement.classList.add("dark");
+    if (localStorage.theme === "true") {
+      themeSetter(true);
+    } else {
+      themeSetter(false);
     }
   }, []);
 
-  console.log(toggle);
-
-  document.documentElement.classList.add("dark");
+  const themeSetter = (condition) => {
+    if (condition) {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = true;
+      setToggle(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = false;
+      setToggle(false);
+    }
+  };
 
   return (
     <div className="flex justify-between items-center w-[112px]">
@@ -25,17 +34,13 @@ const ModeSwitcher = () => {
         <div
           className="group w-[48px] h-[24px] flex items-center bg-white rounded-full p-[5px] cursor-pointer"
           onClick={() => {
-            setToggle(!toggle);
-            localStorage.theme = !toggle;
-            toggle
-              ? document.documentElement.classList.remove("dark")
-              : document.documentElement.classList.add("dark");
+            themeSetter(!toggle);
           }}
         >
           <div
             className={
               "bg-violet-dark hover:bg-violet-light h-[14px] w-[14px] rounded-full transform duration-[400ms] ease-in-out" +
-              (toggle ? null : toggleClass)
+              (toggle ? toggleClass : null)
             }
           ></div>
         </div>
