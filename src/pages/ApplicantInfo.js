c1ae153fb5fd8f7b551a31cea234a2edf9234815
Abdulvokhid <mr.ochilov1997@gmail.com>
2022-01-20 04:25:54 +0900

    connect api

--- src/pages/ApplicantInfo.js
+++ src/pages/ApplicantInfo.js
@@ -1,50 +1,50 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import Context from "../store/context";
 
 const ApplicantInfo = () => {
+  const ctx = useContext(Context);
+
   return (
-    <div className="-mt-10 min-w-[330px] max-w-[1110px] lg:mx-auto md:mx-[40px] mx-[24px] bg-white shadow overflow-hidden rounded">
-      <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg leading-6 font-medium text-gray-900">
-          Applicant Information
-        </h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">
-          Personal details and application.
-        </p>
+    <div className="-mt-10 min-w-[330px] max-w-[1110px] lg:mx-auto md:mx-[40px] mx-[24px] bg-white dark:bg-blue-dark shadow overflow-hidden rounded">
+      <div className="flex items-center px-4 py-5 sm:px-6">
+        <Link to="#" class="block relative">
+          <img
+            alt="profile"
+            src="https://images.unsplash.com/photo-1600180758890-6b94519a8ba6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZSUyMHBob3RvfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            class="mx-auto object-cover rounded-full h-16 w-16 "
+          />
+        </Link>
+        <div className="ml-3">
+          <h3 className="text-lg leading-6 font-medium dark:text-white">
+            User Information
+          </h3>
+          <p className="mt-1 max-w-2xl text-grey-dark">
+            Personal details and resume.
+          </p>
+        </div>
       </div>
       <div className="border-t border-gray-200">
         <dl>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Full name</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              Margot Foster
+          <div className="bg-gray-50 dark:bg-blue-dark px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="font-medium text-grey-dark">Full name</dt>
+            <dd className="mt-1 dark:text-white sm:mt-0 sm:col-span-2">
+              {`${ctx.userData.firstName} ${ctx.userData.lastName}`}
             </dd>
           </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
-              Application for
-            </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              Backend Developer
+
+          <div className="bg-gray-50 dark:bg-blue-dark px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className=" font-medium text-grey-dark">Email address</dt>
+            <dd className="mt-1 dark:text-white sm:mt-0 sm:col-span-2">
+              {ctx.userData.email}
             </dd>
           </div>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Email address</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              margotfoster@example.com
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
-              Salary expectation
-            </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              $120,000
-            </dd>
-          </div>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">About</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+
+          <div className="bg-gray-50 dark:bg-blue-dark px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className=" font-medium text-grey-dark">About</dt>
+            <dd className="mt-1 dark:text-white sm:mt-0 sm:col-span-2">
               Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
               incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
               consequat sint. Sit id mollit nulla mollit nostrud in ea officia
@@ -52,48 +52,28 @@ const ApplicantInfo = () => {
               reprehenderit deserunt qui eu.
             </dd>
           </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Attachments</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
+          <div className="bg-white dark:bg-blue-dark border-t border-gray-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className=" font-medium text-grey-dark">Attachments</dt>
+            <dd className="mt-1  dark:text-white sm:mt-0 sm:col-span-2">
+              <ul className="border border-grey-med dark:border-grey-hover rounded divide-y divide-grey-med">
                 <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
                   <div className="w-0 flex-1 flex items-center">
                     <FontAwesomeIcon
                       icon={faPaperclip}
-                      className="flex-shrink-0 h-5 w-5 text-gray-400"
+                      className="flex-shrink-0 h-5 w-5 text-grey-med"
                       aria-hidden="true"
                     />
-                    <span className="ml-2 flex-1 w-0 truncate">
+                    <span className="ml-2 flex-1 w-0 truncate dark:text-white">
                       resume_back_end_developer.pdf
                     </span>
                   </div>
                   <div className="ml-4 flex-shrink-0">
-                    <a
-                      href="#"
+                    <Link
+                      to="#"
                       className="font-medium text-indigo-600 hover:text-indigo-500"
                     >
                       Download
-                    </a>
-                  </div>
-                </li>
-                <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                  <div className="w-0 flex-1 flex items-center">
-                    <FontAwesomeIcon
-                      icon={faPaperclip}
-                      className="flex-shrink-0 h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <span className="ml-2 flex-1 w-0 truncate">
-                      coverletter_back_end_developer.pdf
-                    </span>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a
-                      href="#"
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Download
-                    </a>
+                    </Link>
                   </div>
                 </li>
               </ul>
